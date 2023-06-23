e9565dab19212bd369257b8be949c06c74a8ac51
Luis Rivas <luisricardorivasgiwencer@gmail.com>
2023-06-23 14:49:03 -0400

    app finished

--- flutter_application_api/lib/pages/home_screen.dart
+++ flutter_application_api/lib/pages/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_api/models/rick_model.dart';
 import 'package:flutter_application_api/provider/rick_provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,34 +11,88 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final rickProvider = RickProvider();
+
+  get http => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: rickProvider.getCharacter(),
+        child: const Text('Get'),
+        onPressed: () {
+          rickProvider.getCharacters();
+        },
       ),
-      body: const Padding(
-        padding: EdgeInsets.only(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(
           left: 20,
           right: 20,
           top: 60,
         ),
-        child: Column(
-          children: [_characterSection()],
+        child: Container(
+          child: _characterScetion(),
         ),
       ),
     );
   }
-}
 
-class _characterSection extends StatelessWidget {
-  const _characterSection();
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _characterScetion() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [],
+      children: [
+        const Text('Characters'),
+        const SizedBox(
+          height: 10,
+        ),
+        AnimatedBuilder(
+          animation: rickProvider,
+          builder: (context, child) {
+            if (rickProvider.requestStatus == RequestStatus.loading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (rickProvider.requestStatus == RequestStatus.failed) {
+              return const Center(
+                child: Text('Something happened fetching movies'),
+              );
+            }
+
+            return SizedBox(
+              height: 500,
+              child: _moviesCardsList(rickProvider.characters),
+            );
+          },
+        ),
+        FloatingActionButton(
+          onPressed: () async {
+            var url = Uri.https('example.com', 'whatsit/create');
+            var response = await http.post(url, body: {'name': 'Luis'});
+          },
+          child: const Text('post'),
+        )
+      ],
+    );
+  }
+
+  Widget _moviesCardsList(List<RickModel> characters) {
+    return ListView.separated(
+      itemCount: characters.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        final character = characters[index];
+
+        return _movieCard(character);
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 10),
+    );
+  }
+
+  Widget _movieCard(RickModel character) {
+    return ListTile(
+      title: Text(character.name),
     );
   }
 }
--- flutter_application_api/lib/provider/rick_provider.dart
+++ flutter_application_api/lib/provider/rick_provider.dart
@@ -2,18 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_api/models/rick_model.dart';
 import 'package:flutter_application_api/services/rick_service.dart';
 
+enum RequestStatus {
+  none,
+  loading,
+  loaded,
+  failed,
+}
+
 class RickProvider extends ChangeNotifier {
-  RickProvider({this.characters = const <RickModel>[]}) {
-    getCharacter();
-  }
+  RickProvider({
+    this.characters = const <RickModel>[],
+    this.requestStatus = RequestStatus.none,
+  });
 
   List<RickModel> characters;
+  RequestStatus requestStatus;
 
   final rickService = RickService();
 
-  getCharacter() async {
-    final responseRick = await rickService.getCharacters();
-    characters = responseRick;
-    notifyListeners();
+  getCharacters() async {
+    try {
+      requestStatus = RequestStatus.loading;
+      notifyListeners();
+      await Future.delayed(const Duration(seconds: 3));
+      final responseMovies = await rickService.getCharacters();
+      characters = responseMovies;
+      requestStatus = RequestStatus.loaded;
+      notifyListeners();
+    } catch (exception) {
+      requestStatus = RequestStatus.failed;
+      notifyListeners();
+    }
   }
 }
